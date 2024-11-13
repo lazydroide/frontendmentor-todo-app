8c56397efdf0446592b8f5ea75ac7736e931fb91
lazydroide <lazydroide@gmail.com>
2024-11-13 15:37:30 +0100

    feat: drag and drop touch screens

--- todoapp/static/js/drag.js
+++ todoapp/static/js/drag.js
@@ -13,6 +13,18 @@ const setPosition = (e) => {
     todosContainer.insertBefore(draggingTodo, nextTodo);
 }
 
+const setPositionMobile = (e) => {
+    e.preventDefault();
+    const draggingTodo = todosContainer.querySelector('.is-dragging');
+    const otherTodos = [...todosContainer.querySelectorAll('.todo:not(.is-dragging)')];
+    
+    let nextTodo = otherTodos.find( otherTodo => {
+        return e.targetTouches[0].clientY <= otherTodo.offsetTop + otherTodo.offsetHeight / 2;
+    });
+    
+    todosContainer.insertBefore(draggingTodo, nextTodo);
+}
+
 draggables.forEach( todo => {
     todo.addEventListener('dragstart', () => {
         todo.classList.add('is-dragging');
@@ -20,11 +32,22 @@ draggables.forEach( todo => {
     todo.addEventListener('dragend', () => {
         todo.classList.remove('is-dragging');
     })
+
+    todo.addEventListener('touchstart', (e) => {        
+        e.preventDefault();
+        todo.classList.add('is-dragging');
+    })
+    todo.addEventListener('touchend', () => {
+        todo.classList.remove('is-dragging');
+    });
 })
 
+
+
 todosContainer.addEventListener('dragover', setPosition)
+todosContainer.addEventListener('touchmove', setPositionMobile)
 
 
 
 //  [x] drag and drop
-//  [ ] drag and drop mobile
+//  [ ] drag and drop mobile e.clientY en mobile
