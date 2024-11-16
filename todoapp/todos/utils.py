a13f5009766486e9ffac7b5e57c778d0961d2966
lazydroide <lazydroide@gmail.com>
2024-11-16 20:53:07 +0100

    fix: on delete update positions before storing

--- todoapp/todos/utils.py
+++ todoapp/todos/utils.py
@@ -1,7 +1,12 @@
 import json
 from .models import Todo
 
-def updatePositions(request):
+def updatePositions(request, id=None):
     _positions = json.loads(request.body)
+    if id:
+        _positions = {key: value for key, value in _positions.items() if value != id}
+    
+    if not _positions: return True
+
     _todos = [Todo(todo_id=_id, position=float(_position)) for _position, _id in _positions.items()]
     return Todo.objects.bulk_update(_todos, ['position'])
